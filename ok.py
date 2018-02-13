1312710dfb6d219c29ad7cf18e8b408edc9ccea6
amjayy <33906054+amjayy@users.noreply.github.com>
2018-02-13 16:06:58 -0600

    Add files via upload

--- ok.py
+++ ok.py
@@ -1,7 +1,8 @@
+from ringtone import club
+from textwrap import dedent
 def gender_choice():
     gender= ''
     while gender != "male" and gender !="female":
-        print("I did not catch that. Please type choose: 1 for male or 2 for female")
         gender = input("Which will you choose?") 
         print()
 
@@ -10,7 +11,6 @@ def gender_choice():
 def your_gender():
     you = ''
     while you != "male" and you !="female":
-        print("I did not catch that. Please type choose: 1 for male or 2 for female")
         you = input("Which will you choose?") 
         print()
 
@@ -23,16 +23,26 @@ def choose_now():
         print("They move into the back of the train.")
         print("You stay staring for so long that when the train stops. You are escorted off by some friendly people in white jackets.")
     return choose
+def your_ride_home():
+    ride = ''
+    while ride != "mom" or ride !="friend":
+        print("Well, I guess you're going to walk home.")
+    return ride
 
-print("""The train ride home is long, but at this hour is peaceful. You have just return from a year in Fort Benning for basic training.You are tired, but proud of yourself. You are not the same person who left.
+
+
+
+
+print("""The train ride home is long, but at this hour it is peaceful. You have just return from a year in Fort Benning for basic training.You are tired, but proud of yourself. You are not the same person who left.
     The sound of wailing babies has ceased and the noisy people on their phones are no more.
-    Besides you and an older couple. The train is quite bare . You are about to doze off to sleep when "Club" by Dej Loaf breaks the silence of the train.
-    The couple looks over at you in disapproval.What do you glare or apologize? 
-    """)
+    Besides you and an older couple. The train is quite bare . You are about to doze off to sleep when...""")
 
+club.phone_ringing()
 
+print("You quickly shut off your phone. You silently curse yourself for being still stuck in 2010 still using songs as ringtones.")
+print("You give the couple on the train an awkward smile.")
+print("""But, the still couple looks over at you in disapproval.What do you do? Do you  glare back or apologize?""")
 
-    
 choice= input("> ")
 
 if  choice == "glare":
@@ -49,7 +59,8 @@ if choice == "Apologize":
 
     
         
-    print(" You decide to check who called you. You have two missed calls and two messages from your childhood friend and your mother.")
+    
+print(" You decide to check who called you. You have two missed calls and two messages from your childhood friend and your mother.")
 
 print("By the way, is your childhood friend a male or a female?")
 
@@ -57,3 +68,25 @@ gender = gender_choice()
 
 print("By the way, what is your gender ?")
 you = your_gender()
+
+print("Who do you want to pick you up from the station?")
+ride = your_ride_home()
+
+person = input("> ")
+
+if ride == "mom":
+    print(dedent("""
+    About 15 minutes later, your train ride comes to a stop. You grab all your items and slip your phone into your pocket.
+    You bound off the terminal to see your mom standing there with a large teddy bear that's holding a red heart. 
+    You grin and run to you mother. She asks you where you want to eat at before going to the neighborhood. What do you choose?
+    """))
+
+elif ride == "friend":
+    print(dedent(""" About 15 minutes later, your train ride comes to a stop. You grab all your tiems and slip your phone into your pocket. You are
+    about to bound off the terminal when you hear some a familiar loud country voice shout out. "What's good biscuit head?" You whip around in
+    excitement. Your shoulders sag in slight disappointment. It isn't them, but it's only their sibling. A great friend as well, but you thought..
+    Wait a second, what was the person you were expecting's name?
+    """))
+
+else:
+    print("You fail to text back anyone, so they assume")
